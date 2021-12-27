91e2610ea5dd0fb5232b848bd3a6545c4f2f8284
Gayk <monika.m.gaik@gmail.com>
2021-12-27 20:24:25 +0100

    Update ShoppingCart.java

--- src/main/java/com/pluralsight/ShoppingCart.java
+++ src/main/java/com/pluralsight/ShoppingCart.java
@@ -60,5 +60,14 @@ public class ShoppingCart {
   }
 
  }
+ public void updateCartItem(int index, int quantity){
+  try {
+   CartItem cartItem = cartItems.get(index);
+   cartItem.setQuantity(quantity);
+  }
+  catch (IndexOutOfBoundsException e ){
+   e.printStackTrace();
+  }
+ }
 
 }
